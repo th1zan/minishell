f7aa3f60a7bfd8cbed9368d2160a631d03e73398
th1zan <thibault.sanglard@gmail.com>
2023-08-08 20:22:38 +0200

    ajout des fct de check

--- src/minishell.c
+++ src/minishell.c
@@ -6,25 +6,40 @@
 /*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 15:14:08 by thibault          #+#    #+#             */
-/*   Updated: 2023/08/07 11:46:49 by thibault         ###   ########.fr       */
+/*   Updated: 2023/08/08 20:17:43 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// int	main(int argc, char **argv, char **envp)
 int	main(void)
 {
+	char *input;
 
-	ft_coucou();
-	
-	/*fonction ft_example se trouve dans my_function_folder/my_function.c,
-	son prototype est dans my_header_for_my_function.h*/
+	while (1) {
+		input = readline("minishell> ");
+		if (check_input(input))
+			break;
+		printf("Vous avez entré : %s\n", input);
+		if (is_double_amp(input, 0))
+			printf("Vous avez entré un charactère de type opérateur de controle \n");
+		free(input);
+	}
 
-	ft_example();
 	
 
 	return(0);
 
+}
+
+int	check_input(char *input)
+{
+	if (input == NULL)
+		return(1);
+	if (*input == 0)
+		return (1);
+	return (0);
 }
 
 int	ft_coucou(void)
